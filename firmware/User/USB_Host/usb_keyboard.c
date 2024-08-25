707cf7fe45e7d87a10cd44a6b69627fa64b0431e
arek <agnieszkaarek@gmail.com>
2024-08-25 21:21:07 +0100

    KEY in Amiga Port missing

--- firmware/User/USB_Host/usb_keyboard.c
+++ firmware/User/USB_Host/usb_keyboard.c
@@ -30,20 +30,20 @@ static USB_Status USBH_HID_KeyboardDecode(Interface *Itf)
 
 	if (FifoRead(&Itf->buffer, &keybd_report_data, Itf->HidRptLen) !=0)
     {
+	    if(keybd_report_data[0]   &   0b00000001) { keybd_info.lctrl  = 1;} else {keybd_info.lctrl    = 0;}
+	    if(keybd_report_data[0]   &   0b00000010) { keybd_info.lshift = 1;} else {keybd_info.lshift   = 0;}
+	    if(keybd_report_data[0]   &   0b00000100) { keybd_info.lalt   = 1;} else {keybd_info.lalt     = 0;}
+	    if(keybd_report_data[0]   &   0b00001000) { keybd_info.lgui   = 1;} else {keybd_info.lgui     = 0;}
+	    if(keybd_report_data[0]   &   0b00010000) { keybd_info.rctrl  = 1;} else {keybd_info.rctrl    = 0;}
+	    if(keybd_report_data[0]   &   0b00100000) { keybd_info.rshift = 1;} else {keybd_info.rshift   = 0;}
+	    if(keybd_report_data[0]   &   0b01000000) { keybd_info.ralt   = 1;} else {keybd_info.ralt     = 0;}
+	    if(keybd_report_data[0]   &   0b10000000) { keybd_info.rgui   = 1;} else {keybd_info.rgui     = 0;}
 
-	        keybd_info.lctrl = keybd_report_data[0]  & 0b00000001;
-	        keybd_info.lshift = keybd_report_data[0] & 0b00000010;
-	        keybd_info.lalt = keybd_report_data[0]   & 0b00000100;
-	        keybd_info.lgui = keybd_report_data[0]   & 0b00001000;
-	        keybd_info.rctrl = keybd_report_data[0]  & 0b00010000;
-	        keybd_info.rshift = keybd_report_data[0] & 0b00100000;
-	        keybd_info.ralt = keybd_report_data[0]   & 0b01000000;
-	        keybd_info.rgui = keybd_report_data[0]   & 0b10000000;
+	     for (x = 0; x < 6; x++)
+	     {
+	       keybd_info.keys[x] = keybd_report_data[x+2] ;
+	     }
 
-	        for (x = 0; x < 6; x++)
-	        {
-	            keybd_info.keys[x] = keybd_report_data[x+2] ;
-	        }
     return USB_OK;
   }
   return   USB_FAIL;
